9d9826afbfd98575894afde15e21825121431b28
johovenn <110096623+Johovenn@users.noreply.github.com>
2025-01-07 15:56:16 +0700

    implement scrollable for meal detail modal

--- src/components/meal-planner/MealDetailModal.tsx
+++ src/components/meal-planner/MealDetailModal.tsx
@@ -7,14 +7,14 @@ import { Skeleton } from "../ui/skeleton"
 import { BeefIcon, DropletIcon, FlameIcon, WheatIcon } from "lucide-react"
 import { getMealById } from "@/app/(app)/search/api/getMealById"
 
-interface MealDetailModalProps{
+interface MealDetailModalProps {
     isOpen: boolean
     handleClose: () => void
     setIsOpen: (value: boolean) => void
     mealId: number | undefined
 }
 
-export default function MealDetailModal(props: MealDetailModalProps){
+export default function MealDetailModal(props: MealDetailModalProps) {
     const [isLoading, setIsLoading] = useState(false)
     const [mealData, setMealData] = useState<any>()
 
@@ -22,9 +22,9 @@ export default function MealDetailModal(props: MealDetailModalProps){
         const getDetail = async () => {
             setIsLoading(true)
 
-            if(props.mealId){
+            if (props.mealId) {
                 await getMealById(props.mealId).then((response) => {
-                    if(response.data){
+                    if (response.data) {
                         setMealData(response.data)
                     }
                 })
@@ -32,17 +32,17 @@ export default function MealDetailModal(props: MealDetailModalProps){
 
             setIsLoading(false)
         }
-        
-        if(props.isOpen){
+
+        if (props.isOpen) {
             getDetail()
         } else {
             setMealData(null)
         }
     }, [props.isOpen, props.mealId])
 
-    return(
+    return (
         <Dialog open={props.isOpen} onOpenChange={props.setIsOpen}>
-            <DialogContent>
+            <DialogContent className="max-h-[80vh] max-w-[600px] overflow-y-auto"> {/* Enable scroll */}
                 <DialogHeader>
                     <DialogTitle>
                         Meal Detail
@@ -50,62 +50,52 @@ export default function MealDetailModal(props: MealDetailModalProps){
                 </DialogHeader>
                 <div className="flex gap-5">
                     <div className="w-[200px] h-[200px]">
-                        {
-                            mealData
-                                ?
+                        {mealData ? (
                             <CldImage
                                 src={mealData.meal_image}
                                 width={200}
                                 height={200}
                                 alt={mealData.meal_name}
-                                className="object-cover rounded-lg"
+                                className="object-cover rounded-lg w-[200px] h-[200px]"
                                 crop={"fill"}
                                 gravity="center"
                             />
-                                :
+                        ) : (
                             <Skeleton className="h-full w-full"/>
-                        }
+                        )}
                     </div>
-                    <div className="">
-                        {mealData ? <h4 className="text-lg font-medium">{mealData.meal_name}</h4> : <Skeleton className="w-[200px] h-[30px]"/>}
-                        {
-                            mealData 
-                                ?
+                    <div className="max-w-[50%]">
+                        {mealData ? <h4 className="text-lg font-medium">{mealData.meal_name}</h4> : <Skeleton className="w-[200px] h-[30px]" />}
+                        {mealData ? (
                             <div className="space-y-2 mt-2">
-                                <p className="text-sm text-gray-700 flex item-center w-[150px] gap-1"><FlameIcon size={18}/>{mealData.meal_calories} calories</p>
-                                <p className="text-sm text-gray-700 flex item-center w-[150px] gap-1"><BeefIcon size={18}/>{mealData.meal_protein} protein</p>
-                                <p className="text-sm text-gray-700 flex item-center w-[150px] gap-1"><WheatIcon size={18}/>{mealData.meal_carbohydrate} carbohydrate</p>
-                                <p className="text-sm text-gray-700 flex item-center w-[150px] gap-1"><DropletIcon size={18}/>{mealData.meal_fat} fat</p>
+                                <p className="text-sm text-gray-700 flex item-center w-[150px] gap-1"><FlameIcon size={18} />{mealData.meal_calories} calories</p>
+                                <p className="text-sm text-gray-700 flex item-center w-[150px] gap-1"><BeefIcon size={18} />{mealData.meal_protein} protein</p>
+                                <p className="text-sm text-gray-700 flex item-center w-[150px] gap-1"><WheatIcon size={18} />{mealData.meal_carbohydrate} carbohydrate</p>
+                                <p className="text-sm text-gray-700 flex item-center w-[150px] gap-1"><DropletIcon size={18} />{mealData.meal_fat} fat</p>
                             </div>
-                                :
-                            <Skeleton className="w-[200px] h-[155px]"/>
-                        }
-                        {
-                            mealData
-                                ?
+                        ) : (
+                            <Skeleton className="w-[200px] h-[155px]" />
+                        )}
+                        {mealData ? (
                             <div className="mt-4">
                                 <h5 className="font-medium">Ingredients :</h5>
                                 <ul className="list-inside list-disc ml-1 space-y-1">
-                                    {
-                                        mealData.meal_ingredients.split(", ").map((item: string) => (
-                                            <li key={item} className="text-sm">{item}</li>
-                                        ))
-                                    }
+                                    {mealData.meal_ingredients.split(", ").map((item: string) => (
+                                        <li key={item} className="text-sm">{item}</li>
+                                    ))}
                                 </ul>
                             </div>
-                                :
-                            <Skeleton className="w-[200px] h-[155px]"/>
-                        }
-                        {
-                            mealData
-                                ?
+                        ) : (
+                            <Skeleton className="w-[200px] h-[155px]" />
+                        )}
+                        {mealData ? (
                             <div className="mt-4">
                                 <h5 className="font-medium">How to cook :</h5>
                                 <p className="text-sm">{mealData.meal_recipe}</p>
                             </div>
-                                :
-                            <Skeleton className="w-[200px] h-[100px]"/>
-                        }
+                        ) : (
+                            <Skeleton className="w-[200px] h-[100px]" />
+                        )}
                     </div>
                 </div>
             </DialogContent>
